fc240bb438b150d54ced72beb6e6759eceb03778
AshutoshNandanwar <95475646+ashnandanwar@users.noreply.github.com>
2023-10-07 00:25:55 +0530

    Main Menu & Enemy Explosion

---
+++ Assets/Scripts/Main_Menu/MainMenu.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void LoadGame()
+    {
+        SceneManager.LoadScene(1); // Loading game scene
+
+    }
+}
